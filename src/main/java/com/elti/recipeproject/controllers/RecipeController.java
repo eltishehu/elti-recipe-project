b2c38f189be5810a0c6399bb60ed9ac46ee6fd2b
Elti Shehu <shehuelti@gmail.com>
2018-10-05 15:26:25 +0200

    Added 404 error page

--- src/main/java/com/elti/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/elti/recipeproject/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.elti.recipeproject.controllers;
 
 import com.elti.recipeproject.commands.RecipeCommand;
+import com.elti.recipeproject.exceptions.NotFoundException;
 import com.elti.recipeproject.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by e.sh. on 03-Oct-18
@@ -36,6 +39,7 @@ public class RecipeController {
 
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
+
         model.addAttribute("recipe", recipeService.findCommandById(new Long(id)));
         return  "recipe/recipeform";
     }
@@ -54,5 +58,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound() {
+
+        log.error("Handling NOT FOUND exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
+
     }
 }
