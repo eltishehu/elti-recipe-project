e5d73a96950fea38a42ce0d93ed66f1035f37d00
Elti Shehu <shehuelti@gmail.com>
2018-10-08 12:12:42 +0200

    Added Contoller Advice and also data validation with Spring MVC.

--- src/main/java/com/elti/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/elti/recipeproject/controllers/RecipeController.java
@@ -7,8 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 /**
  * Created by e.sh. on 03-Oct-18
@@ -17,6 +20,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RecipeController {
 
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
     private final RecipeService recipeService;
 
     public RecipeController(RecipeService recipeService) {
@@ -41,12 +45,19 @@ public class RecipeController {
     public String updateRecipe(@PathVariable String id, Model model) {
 
         model.addAttribute("recipe", recipeService.findCommandById(new Long(id)));
-        return  "recipe/recipeform";
+        return  RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand, BindingResult bindingResult) {
 
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+
+            return RECIPE_RECIPEFORM_URL;
+        }
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
     }
@@ -73,19 +84,4 @@ public class RecipeController {
 
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception) {
-
-        log.error("Handling NUMBER FORMAT exception");
-        log.error(exception.getMessage());
-
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-
-    }
 }
